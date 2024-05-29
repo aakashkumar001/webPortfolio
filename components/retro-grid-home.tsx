d88b9be5bfbc6a8bbdf69661289795b2a40555ca
aakashkr <aspakash000@gmail.com>
2024-05-29 13:30:39 +0530

    v3 production build

--- components/retro-grid-home.tsx
+++ components/retro-grid-home.tsx
@@ -11,8 +11,8 @@ import { TextRevealCardPreview } from "./text-reveal-card-text";
 const RetroGridHome = () => {
   return (
     <div className="relative flex h-full w-full items-center justify-center overflow-hidden rounded-lg bg-background p-20 md:shadow-xl">
-      <span className="absolute left-12 lg:left-8 xl:left-8 top-8 lg:top-10 xl:top-10 text-center text-5xl lg:text-6xl xl:text-6xl">✋</span>
-      <span className="absolute left-28 top-10 mb-96 pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff] bg-clip-text text-center text-5xl lg:text-7xl xl:7xl font-bold leading-none tracking-tighter text-transparent">
+      <span className="absolute left-10 lg:left-8 xl:left-8 top-8 lg:top-10 xl:top-10 text-center text-5xl lg:text-6xl xl:text-6xl">✋</span>
+      <span className="absolute left-24 top-10 mb-96 pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff] bg-clip-text text-center text-5xl lg:text-7xl xl:7xl font-bold leading-none tracking-tighter text-transparent">
         Hi, I'm Aakash 
       </span>
       {/* <span className="text-white">
@@ -21,7 +21,7 @@ const RetroGridHome = () => {
       {/* <span className="text-white">
         "Anything that can be imagined can be programmed." -Alan Kay
       </span> */}
-      <span className="absolute top-28 left-16 lg:top-32 xl:top-32 lg:left-28 xl:left-28 max-w-96"><TextRevealDemo/></span>
+      <span className="absolute top-28 left-14 lg:top-32 xl:top-32 lg:left-28 xl:left-28 max-w-96"><TextRevealDemo/></span>
       
       <div className="hidden lg:absolute lg:right10 xl:right-10 lg:block xl:block w-[600px] ml-72 p-10">
         <OrbitingCirclesDemo />
